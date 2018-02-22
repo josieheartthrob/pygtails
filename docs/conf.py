fcf83944b94c436ac7542eb6616d91ca6ea06306
Josie Thompson <josiahst@gmail.com>
2018-02-21 20:11:10 -0800

    attempt somewhere around 10 for fixing no-doc bug

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,15 @@
 #
 import os
 import sys
-import pygame
-#import pygtails
+import pyygame
 from pygame.locals import *
 from mock import Mock
 
 sys.modules['pygame'] = Mock()
 sys.modules['pygame.constants'] = Mock()
-sys.modules['../src/pygtails'] = Mock()
+sys.modules['pygtails'] = Mock()
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 
 # -- Project information -----------------------------------------------------
